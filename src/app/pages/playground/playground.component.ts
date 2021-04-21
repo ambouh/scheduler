7c90bc8e783429c64d90a26d041dea556cbd20ed
ambouh <ambouh@MacBook-Pro.fios-router.home>
2021-04-21 18:48:38 -0400

    VALID: added validation to time and implemented IDs

--- src/app/pages/playground/playground.component.ts
+++ src/app/pages/playground/playground.component.ts
@@ -1,8 +1,6 @@
-import {Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ScheduleService} from '../../services/schedule.service';
-import {Form, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import { MatChipInputEvent } from '@angular/material/chips';
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
+
 
 export interface Subject {
   name: string;
@@ -13,22 +11,11 @@ export interface Subject {
   styleUrls: ['./playground.component.scss']
 })
 export class PlaygroundComponent implements OnInit {
-  heroForm: FormGroup;
 
-  constructor(private scheduleService: ScheduleService, private fb: FormBuilder, private CFR: ComponentFactoryResolver) {
+
+  constructor(private scheduleService: ScheduleService) {
   }
   ngOnInit(): void {
-    this.heroForm = new FormGroup({
-      name: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-        // forbiddenNameValidator(/bob/i) // <-- Here's how you pass in the custom validator.
-      ]),
-    });
-    console.log(this.name?.errors);
+
   }
-
-  get name() { return this.heroForm.get('name'); }
-
-  get power() { return this.heroForm.get('power'); }
 }
